5de029eef73fb927cbd712d01e52f82a0bc2a366
dbarannik <redcatbox@gmail.com>
2020-02-27 16:32:06 +0200

    splines fixes

--- Plugins/InstancedPlugin/Source/InstancedPlugin/Private/Actors/IPSplineMeshActor.cpp
+++ Plugins/InstancedPlugin/Source/InstancedPlugin/Private/Actors/IPSplineMeshActor.cpp
@@ -21,8 +21,6 @@ void AIPSplineMeshActor::OnConstruction(const FTransform& Transform)
 #if WITH_EDITOR
 	RunGeneration();
 #endif
-
-	RegisterAllComponents();
 }
 
 #if WITH_EDITOR
@@ -31,67 +29,72 @@ void AIPSplineMeshActor::RunGeneration()
 	int32 SplinePointsNumber = SplineComponent->GetNumberOfSplinePoints();
 	bool bLoop = SplineComponent->IsClosedLoop();
 
-	if (SplineMeshComponents.Num() > 1)
+	if (SplineMeshComponents.Num() > 0)
 	{
 		for (int32 i = 1; i < SplineMeshComponents.Num(); i++)
 		{
+			SplineMeshComponents[i]->DetachFromComponent(FDetachmentTransformRules::KeepWorldTransform);
 			SplineMeshComponents[i]->UnregisterComponent();
 			SplineMeshComponents[i]->DestroyComponent();
 		}
+
+		SplineMeshComponents.Empty();
 	}
 
-	SplineMeshComponents.Empty();
-
-	if (!bLoop)
+	if (SplinePointsNumber > 1)
 	{
-		SplinePointsNumber = SplinePointsNumber - 1;
-	}
-
-	for (int32 i = 0; i < SplinePointsNumber; i++)
-	{
-		//Create new component
-		int32 IndexCurrent = i;
-		int32 IndexNext = IndexCurrent + 1;
-		FName ComponentName = *(FString(TEXT("SplineMeshComponent")).Append(FString::SanitizeFloat(FPlatformTime::Seconds())));
-
-		if (i == 0)
+		if (!bLoop)
 		{
-			SplineMeshComponents.Add(SplineMeshComponent);
-		}
-		else
-		{
-			SplineMeshComponents.Insert(DuplicateObject<USplineMeshComponent>(SplineMeshComponent, this, ComponentName), IndexCurrent);
-			SplineMeshComponents[i]->SetupAttachment(RootComponent);
-			SplineMeshComponents[i]->RegisterComponent();
+			SplinePointsNumber = SplinePointsNumber - 1;
 		}
 
-		if (bLoop && i == SplinePointsNumber - 1)
+		for (int32 i = 0; i < SplinePointsNumber; i++)
 		{
-			IndexNext = 0;
-		}
+			//Create new component
+			int32 IndexCurrent = i;
+			int32 IndexNext = IndexCurrent + 1;
+			FName ComponentName = *(FString(TEXT("SplineMeshComponent")).Append(FString::SanitizeFloat(FPlatformTime::Seconds())));
 
-		if (SplineMeshComponents[IndexCurrent])
-		{
-			//Location
-			FVector StartPos;
-			FVector StartTangent;
-			FVector EndPos;
-			FVector EndTangent;
-			SplineComponent->GetLocationAndTangentAtSplinePoint(IndexCurrent, StartPos, StartTangent, ESplineCoordinateSpace::Local);
-			SplineComponent->GetLocationAndTangentAtSplinePoint(IndexNext, EndPos, EndTangent, ESplineCoordinateSpace::Local);
-			SplineMeshComponents[IndexCurrent]->SetStartAndEnd(StartPos, StartTangent, EndPos, EndTangent, true);
+			if (i == 0)
+			{
+				SplineMeshComponents.Add(SplineMeshComponent);
+			}
+			else
+			{
+				SplineMeshComponents.Insert(DuplicateObject<USplineMeshComponent>(SplineMeshComponent, this, ComponentName), IndexCurrent);
+				SplineMeshComponents[i]->CreationMethod = EComponentCreationMethod::UserConstructionScript;
+				SplineMeshComponents[i]->SetupAttachment(RootComponent);
+				SplineMeshComponents[i]->RegisterComponent();
+			}
 
-			//Rotation
-			float StartRoll = SplineComponent->GetRotationAtSplinePoint(IndexCurrent, ESplineCoordinateSpace::Local).Roll;
-			float EndRoll = SplineComponent->GetRotationAtSplinePoint(IndexNext, ESplineCoordinateSpace::Local).Roll;
-			SplineMeshComponents[IndexCurrent]->SetStartRoll(StartRoll, true);
-			SplineMeshComponents[IndexCurrent]->SetEndRoll(EndRoll, true);
+			if (bLoop && i == SplinePointsNumber - 1)
+			{
+				IndexNext = 0;
+			}
 
-			//Scale
-			FVector2D StartScale = FVector2D(SplineComponent->GetScaleAtSplinePoint(IndexCurrent));
-			FVector2D EndScale = FVector2D(SplineComponent->GetScaleAtSplinePoint(IndexNext));
-			SplineMeshComponents[IndexCurrent]->SetStartScale(StartScale);
-			SplineMeshComponents[IndexCurrent]->SetEndScale(EndScale);
+			if (SplineMeshComponents[IndexCurrent])
+			{
+				//Location
+				FVector StartPos;
+				FVector StartTangent;
+				FVector EndPos;
+				FVector EndTangent;
+				SplineComponent->GetLocationAndTangentAtSplinePoint(IndexCurrent, StartPos, StartTangent, ESplineCoordinateSpace::Local);
+				SplineComponent->GetLocationAndTangentAtSplinePoint(IndexNext, EndPos, EndTangent, ESplineCoordinateSpace::Local);
+				SplineMeshComponents[IndexCurrent]->SetStartAndEnd(StartPos, StartTangent, EndPos, EndTangent, true);
+
+				//Rotation
+				float StartRoll = SplineComponent->GetRotationAtSplinePoint(IndexCurrent, ESplineCoordinateSpace::Local).Roll;
+				float EndRoll = SplineComponent->GetRotationAtSplinePoint(IndexNext, ESplineCoordinateSpace::Local).Roll;
+				SplineMeshComponents[IndexCurrent]->SetStartRoll(StartRoll, true);
+				SplineMeshComponents[IndexCurrent]->SetEndRoll(EndRoll, true);
+
+				//Scale
+				FVector2D StartScale = FVector2D(SplineComponent->GetScaleAtSplinePoint(IndexCurrent));
+				FVector2D EndScale = FVector2D(SplineComponent->GetScaleAtSplinePoint(IndexNext));
+				SplineMeshComponents[IndexCurrent]->SetStartScale(StartScale);
+				SplineMeshComponents[IndexCurrent]->SetEndScale(EndScale);
+			}
 		}
 	}
 
@@ -105,7 +108,6 @@ void AIPSplineMeshActor::PostEditChangeProperty(FPropertyChangedEvent& PropertyC
 {
 	Super::PostEditChangeProperty(PropertyChangedEvent);
 	FName PropertyName = (PropertyChangedEvent.Property != NULL) ? PropertyChangedEvent.Property->GetFName() : NAME_None;
-
 	if (PropertyName == TEXT("Transform"))
 	{
 		OnConstruction(this->GetActorTransform());
